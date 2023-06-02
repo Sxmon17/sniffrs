3aac9190a77bd735a4dfd670e3bdc50a24374d26
Sxmon17 <simonguglberger@gmail.com>
2023-06-02 10:55:17 +0200

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+use pnet::datalink; use pnet::datalink::Channel::Ethernet;
+
+fn main() {
+    let interface = datalink::interfaces().into_iter().filter(|iface| iface.name == "enp4s0").next().expect("Could not find interface");
+
+    let (_, mut rx) = match datalink::channel(&interface, Default::default()) {
+        Ok(Ethernet(tx, rx)) => (tx, rx),
+        Ok(_) => panic!("Unhandled channel type"),
+        Err(e) => panic!("An error occurred when creating the datalink channel: {}", e)
+    };
+
+    loop {
+        match rx.next() {
+            Ok(packet) => {
+                dbg!(&packet);
+                handle_packet(&packet);
+            }
+            Err(e) => {
+                panic!("An error occurred while reading: {}", e);
+            }
+        }
+    }
+}
