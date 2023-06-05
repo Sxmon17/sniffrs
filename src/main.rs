09ea98ce4f901b22c655f23c4162a72560330666
Simon Guglberger <65186979+Sxmon17@users.noreply.github.com>
2023-06-06 00:59:43 +0200

    [REFC] Clean up

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,29 @@
 mod packet;
 
-use pnet::datalink; use pnet::datalink::Channel::Ethernet;
 use crate::packet::Packet;
+use pnet::datalink;
+use pnet::datalink::Channel::Ethernet;
 
 fn main() {
-    let interface = datalink::interfaces().into_iter().filter(|iface| iface.name == "enp4s0").next().expect("Could not find interface");
+    let interface = datalink::interfaces()
+        .into_iter()
+        .filter(|iface| iface.name == "enp4s0")
+        .next()
+        .expect("Could not find interface");
 
     let (_, mut rx) = match datalink::channel(&interface, Default::default()) {
         Ok(Ethernet(tx, rx)) => (tx, rx),
         Ok(_) => panic!("Unhandled channel type"),
-        Err(e) => panic!("An error occurred when creating the datalink channel: {}", e)
+        Err(e) => panic!(
+            "An error occurred when creating the datalink channel: {}",
+            e
+        ),
     };
 
     loop {
         match rx.next() {
             Ok(packet) => {
-                Packet::from_ethernet(&packet);
+                println!("{}", Packet::from_ethernet(&packet));
             }
             Err(e) => {
                 panic!("An error occurred while reading: {}", e);
